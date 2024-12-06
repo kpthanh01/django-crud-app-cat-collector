57ff0138cc5cc60715d832d14b2cc873fa4409d3
kpthanh01 <kphamthanh01@gmail.com>
2024-12-06 13:33:35 -0500

    Added 2 tables: Feeding and Toy; Feeding has a relation to Cat; Toy has full CRUD finished

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 # Create your models here.
 class Cat(models.Model):
   name = models.CharField(max_length=100)
@@ -13,3 +19,30 @@ class Cat(models.Model):
   
   def get_absolute_url(self):
     return reverse('cat-detail', kwargs={'cat_id': self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+    ordering = ['-date']
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+  
+  def get_absolute_url(self):
+      return reverse("toy-detail", kwargs={"pk": self.id})
+  
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Cat
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
 
 # Import HttpResponse to send text-based responses
 # from django.http import HttpResponse  -- this is used temporarily to show a response before we start using html files
@@ -36,6 +38,25 @@ class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats/'
 
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+  success_url = '/toys/'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
 # Define the home view function
 def home(request):
   # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
@@ -50,4 +71,19 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
   cat = Cat.objects.get(id=cat_id)
-  return render(request, 'cats/detail.html', {'cat': cat})
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', {
+    'cat': cat,
+    'feeding_form': feeding_form
+  })
+
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('cat-detail', cat_id=cat_id)
